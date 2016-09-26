628f73503e297e54fec5040d0d06da00d061fd61
Majeed <majeedk526@gmail.com>
2016-09-25 16:19:23 -0800

    feat: added Stack, Types, DFS, Graph Test codes and completed Graph code

--- C_code/Graph.c
+++ C_code/Graph.c
@@ -2,67 +2,78 @@
 #include <stdlib.h>
 #include <stdbool.h> // for bool
 #include <stddef.h> // for NULL
+#include "Types.h"
 
 int i;
 
-typedef struct List{
-	struct Nodes * head;
-} AdjList;
-
-typedef struct Nodes{
-	int _id;
-	struct Nodes * next; //left, right, forward node
-} Node;
-
-typedef struct Graphs{
-	int size;
-	AdjList * adjList;
-} Graph;
+Graph* createGraph(int size);
+AdjListNode* newListNode(AdjListNode *prev, int _id);
+Node* newNode(Node * src, int _id);
+bool addEdge(Graph *graph, int src, int dest);
 
 Graph* createGraph(int size){
 	
+	if(size<=0) {
+		printf("size must be greater than 0.\n");
+		return NULL;
+	}
+	
 	Graph * graph = (Graph*) malloc(sizeof(Graph));
-	
 	graph->size = size;
-	graph->adjList = (AdjList*) malloc(sizeof(size * AdjList));
 	
-	for(i=0; i<size; i++){
-		graph->adjList[i].head = NULL;
-	}	
+	graph->adjListHead = newListNode(NULL,0); //list start reference
 	
-	return true;
+	AdjListNode * tmpListNode = graph->adjListHead; // for traversing
+	Node *tmpNode = tmpListNode->head;
+	
+	for(i=1;i<size;i++){
+		tmpListNode->next = newListNode(tmpListNode,i);
+		tmpListNode = tmpListNode->next;
+	}
+	
+	return graph;
 }
 
-bool addNode(Graph* graph){
-	
-	AdjList *nAdjList = (AdjList) malloc(sizeof(AdjList));
-	nAdjList->head = NULL;
-	graph[graph->size] = nAdjList;
-	graph->size++;
+Node* newNode(Node * src, int _id){
+	Node * node = (Node*) malloc(sizeof(Node));
+	node->_id = _id;
+	node->src = src;
+	node->dest = NULL;
+	return node;
+}
 
-	return true;
-		
+AdjListNode* newListNode(AdjListNode *prev, int _id){
 	
+	AdjListNode * newLNode = (AdjListNode*) malloc(sizeof(AdjListNode));
+	
+	newLNode->prev = prev;
+	newLNode->next = NULL;
+	newLNode->head= newNode(NULL, _id);
+	return newLNode;
 }
 
 bool addEdge(Graph *graph, int src, int dest){
 	
-	Node *node = (Node*) malloc(sizeof(Node));
-	node->_id = dest;
-	node->next = NULL;
+	AdjListNode *tmpListNode = graph->adjListHead;
+	Node *tmpNode;
 	
-	if(graph[src].head == NULL){
-		graph[src].head = node;
-		return true;
+	int s = graph->size;
+	
+	for(i=0; i<src; i++){
+		tmpListNode = tmpListNode->next;
 	}
 	
-	Node *tmp = graph[src].head;
+	tmpNode = tmpListNode->head;
 	
-	while(tmp->next != NULL){
-		tmp = tmp->next;
+	while(tmpNode->dest != NULL){
+		tmpNode = tmpNode->dest;
+		if(tmpNode->_id == dest){
+			printf("path between %d and %d already exists.\n");
+			return false;
+		}
 	}
 	
-	tmp->next = node;
+	tmpNode->dest = newNode(tmpNode,dest);
 	return true;
 }
 
@@ -71,11 +82,19 @@ void printGraph(Graph *graph){
 	int s = graph->size;
 	printf("Size of graph = %d\n", s);
 	
+	if(s==0){ return;}
 	
-	
-	AdjList *tmpList = graph[0];
+	AdjListNode *tmpListNode = graph->adjListHead;
+	Node *tmpNode;
 	for(i=0; i<s; i++){
-		
+		tmpNode = tmpListNode->head;
+		printf("%d -> ", tmpNode->_id);
+		while (tmpNode->dest != NULL){
+			tmpNode = tmpNode->dest;
+			printf("%d , ", tmpNode->_id);
+		}
+		printf("\n");
+		tmpListNode = tmpListNode->next;
 	}
 	
 }
